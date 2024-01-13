f3dd35de1617aa4fa8e0690891f359d3bfcd53dc
NS011618 <ashishgolla2003@gmail.com>
2024-01-13 12:57:47 +0530

    creating files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import './App.css';
-
 function App() {
-  return (
+  return( 
     <div className="App">
-      <header className="App-header">
-        
-          Learn React
-        
-      </header>
+      <h1 className="text-2xl p-5 font-bold bg-blue-200 ">Invoice App</h1>
     </div>
   );
 }
